283872b8269215bfcc7a57fcdc83c15cab771135
py-study <1071147298@qq.com>
2016-11-22 20:38:14 +0800

    加入失去焦点事件

--- lession01/index.js
+++ lession01/index.js
@@ -12,11 +12,16 @@ function onfocusInput() {
         document.getElementById("myinput").value = '';
         onclickInput();
     }
-    else if(v == '')
+}
+
+function onfocusoutInput(){
+    var v = document.getElementById("myinput").value;
+    if(v == '')
     {
 	document.getElementById("myinput").value = '请输入内容';
     }
 }
+
 
 function onclickInput() {
     alert('i\'m clicked \n 老师的作业有个坑，在onfocus事件里的alert，会关不掉！');
