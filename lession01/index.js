9fa893d61492ea83a97f1a956e6c06ec21fc2fd2
py-study <1071147298@qq.com>
2016-11-22 18:04:00 +0800

    Signed-off-by: py-study <1071147298@qq.com>

--- lession01/index.js
+++ lession01/index.js
@@ -1,6 +1,19 @@
-/**
+﻿/**
  * Created by dujiaheng on 2016/11/21.
  */
 function clickMe() {
     alert('i\'m clicked');
+}
+
+function onfocusInput() {
+    var v = document.getElementById("myinput").value;
+    if (v == '请输入内容')
+    {
+        document.getElementById("myinput").value = '';
+        onclickInput();
+    }
+}
+
+function onclickInput() {
+    alert('i\'m clicked \n 老师的作业有个坑，在onfocus事件里的alert，会关不掉！');
 }
