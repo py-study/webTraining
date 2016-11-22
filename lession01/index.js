42f71680e8fe9b5c80374a4139be5c03813adea9
py-study <1071147298@qq.com>
2016-11-22 22:01:38 +0800

    good

--- lession01/index.js
+++ lession01/index.js
@@ -1,28 +1,24 @@
 ﻿/**
  * Created by dujiaheng on 2016/11/21.
  */
-function clickMe() {
-    alert('i\'m clicked');
-}
-
-function onfocusInput() {
-    var v = document.getElementById("myinput").value;
-    if (v == '请输入内容')
-    {
-        document.getElementById("myinput").value = '';
-        onclickInput();
-    }
-}
-
-function onblurInput(){
-    var v = document.getElementById("myinput").value;
-    if(v == '')
-    {
-	document.getElementById("myinput").value = '请输入内容';
-    }
-}
-
-
-function onclickInput() {
+function clickLogin() {
     alert('i\'m clicked \n 老师的作业有个坑，在onfocus事件里的alert，会关不掉！');
 }
+
+function onfocusNameInput() {
+    var v = document.getElementById("nameInput").value;
+    if (v == '您的名字')
+    {
+        document.getElementById("nameInput").value = '';
+    }
+}
+
+function onfocusPasswordInput() {
+    var v = document.getElementById("passwordInput").value;
+    if (v == '8888')
+    {
+        document.getElementById("passwordInput").value = '';
+    }
+}
+
+
